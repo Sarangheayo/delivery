ca45cf900837d8b5cdf785507c9e1cd4411fc6a3
Sarangheayo <myungsoo8504@gmail.com>
2025-12-15 17:26:43 +0900

    251212 personal preview work - delivery service

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * @file src/App.jsx
+ * @description App컴포넌트
+ * 251210 v1.0.0 wook 최초 생성
+ */
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>안녕하세요~</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,6 @@
+/**
+ * @file app.js
+ * @description Entry Point
+ * 251210 v1.0.0 wook 최초 생성
+ */
+
